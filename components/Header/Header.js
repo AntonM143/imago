bb0c0ef05fc5e01e352fab220174c6bd67beb789
robinhlgrsn <robinholgersson91@gmail.com>
2022-01-01 22:23:10 +0100

    -

---
+++ components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Image from 'next/image';
+import classes from './Header.module.scss';
+
+const Header = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default Header
