fead6a326ddc470ed17fcefcd2eb628ebd21bef7
robinhlgrsn <robinholgersson91@gmail.com>
2022-01-03 16:14:13 +0100

    -

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,11 +1,48 @@
 import React from 'react';
 import Image from 'next/image';
 import classes from './Header.module.scss';
+import { FaSearch } from "react-icons/fa";
+import { FiShoppingBag } from 'react-icons/fi';
 
 const Header = () => {
   return (
-    <div></div>
+    <header className={classes.headerContainer}>
+      <h1>HEJ</h1>
+      <section className={classes.headerBar}>
+        <nav className={classes.headerMainNav}>
+          <div>
+            <Image 
+              src="/images/icnimage-logo.png"
+              alt="logo"
+              width={436}
+              height={105}
+            />
+          </div>
+          <ul>
+            <li>Hem</li>
+            <li>Alla Produkter</li>
+            <li>Kategorier</li>
+          </ul>
+        </nav>
+        <nav className={classes.headerSecondaryNav}>
+          <button>
+            <FaSearch />
+          </button>
+          <button>
+            <FiShoppingBag />
+          </button>
+        </nav>
+      </section>
+    </header>
   )
 }
 
 export default Header
+
+
+{/* <Image 
+src="/images/icnimage-logo.png"
+alt="logo"
+width={436}
+height={105}
+/> */}
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div>
-      <h1>Index</h1>
+      <p>Index</p>
     </div>
   )
 }
