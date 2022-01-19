3de2b79d7b1fe7212aef6f98151aaa28bc7bcfba
robinhlgrsn <robinholgersson91@gmail.com>
2022-01-19 15:26:45 +0100

    -

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,6 @@ export default function Home() {
         <Image 
           src={'https://images.unsplash.com/photo-1583847268964-b28dc8f51f92?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80'}
           alt="cover"
-          layout="responsive"
           height={1000}
           width={1000}
           objectFit="cover"
--- pages/success/index.js
+++ pages/success/index.js
@@ -1,45 +1,49 @@
 import { getLocalstorage, clearLocalStorage  } from '@/utils/localstorage';
 import { useRouter } from 'next/router';
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import CartContext from 'store/cart-context';
 import { url_path } from 'config';
 
 const Success = () => {
+	const [cart, setCart] = useState(null)
+	const [orderDetails, setOrderDetails] = useState(null)
 	const { clearCart } = useContext(CartContext);
-	const router = useRouter()
+	const router = useRouter();
 
 	useEffect(() => {
 		// Create PaymentIntent as soon as the page loads
 		const params = new URLSearchParams(window.location.search)
 		let payment_intent = params.get('payment_intent')
-		const cartInLocal = getLocalstorage('cart')
-		const orderDetails = getLocalstorage('checkoutSession')
-
+		setCart(getLocalstorage('cart'))
+		setOrderDetails(getLocalstorage('checkoutSession'))
+		clearCart()
+		clearLocalStorage()
 		if(!payment_intent) {
 			router.push({
 				pathname: '/404'
 			})
 		}
 
-		if (localStorage.getItem("cart") !== null) {
-			let wholeCart = Object.assign(cartInLocal, orderDetails)
+		if (cart !== null) {
+			let wholeCart = Object.assign(cart, orderDetails)
 			async function fetchMyAPI() {
 				/* check if payment_intent exists already */
 				let res = await fetch(`${url_path}/api/success/${payment_intent}`)
 				res = await res.json()
 				/* add to DB */
 				if(res.query !== payment_intent) {
+					console.log(wholeCart);
 					let res = await fetch(`${url_path}/api/success/${payment_intent}`, {
 						method: "POST",
 						headers: { "Content-Type": "application/json" },
 						body: JSON.stringify(wholeCart),
 					})
 					res = await res.json()
-					clearCart()
-					clearLocalStorage()
+					console.log(res)
 				}
 			}
 			fetchMyAPI()
+			
 		  }
 
 	  }, []);
