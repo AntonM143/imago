31c6ee00d55a7bfd5b3fe62fded35b1381cda723
Anton Månsson <72130785+AntonM143@users.noreply.github.com>
2022-02-01 14:49:35 +0100

    Fixed layout for 404 and sucess page

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -3,15 +3,24 @@ import styles from './Hero.module.scss';
 import Image from 'next/image';
 import Button from '../Button/Button';
 import { IoMdArrowForward } from 'react-icons/io';
+import { useRouter } from 'next/router'
+
 const Hero = (props) => {
+	const router = useRouter()
   return (
     <div className={styles.heroContainer}>
       <div className={styles.heroContentContainer}>
         <h1>{props.title}</h1>
-        <Button>{props.btnText}<IoMdArrowForward/></Button>
+        <Button
+			onClick={() => {
+				router.push({
+					pathname: props.buttonHref,
+				})
+			}}
+		>{props.btnText}<IoMdArrowForward/></Button>
       </div>
       <div className={styles.heroImageContainer}>
-        <Image 
+        <Image
           src={'https://images.unsplash.com/photo-1586023492125-27b2c045efd7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3400&q=80'}
           alt="cover"
           height={1080}
--- pages/404.js
+++ pages/404.js
@@ -1,29 +1,19 @@
-import Button from '@/components/Button/Button'
 import Head from 'next/head';
-import { useRouter } from 'next/router'
 import React from 'react';
+import Hero from "@/components/Hero/Hero";
 
 export default function Custom404() {
-	const router = useRouter()
-	//TODO stylea skiten
 	return (
 		<div>
 			<Head>
 				<title>404</title>
 				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
 			</Head>
-			<h3>404 - Vi kunde tyvärr inte hitta sidan...</h3>
-			<Button
-				color={"sand"}
-				onClick={() => {
-					router.push({
-						pathname: `/`,
-					})
-				}}
-			>
-				Startsida
-			</Button>
-
+			<Hero
+				btnText={'Till alla produkter'}
+				title={'404 - Vi kunde tyvärr inte hitta sidan...'}
+				buttonHref={'/allProducts'}
+			/>
 		</div>
 	)
 }
