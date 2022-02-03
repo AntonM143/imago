31ae8be964124f22e7eb837ddff94d4c848c0233
robinhlgrsn <robinholgersson91@gmail.com>
2022-02-03 21:14:15 +0100

    small bugfix

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,11 @@
 import Head from "next/head";
 import Hero from "@/components/Hero/Hero";
-import '../public/imago.svg'
+
 export default function Home() {
   return (
     <>
     	<Head>
 				<title>IMAGO POSTERS</title>
-        <link rel="shortcut icon" type="image/svg+xml" href="/imago.svg" />
 				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
 			</Head>
       <Hero title="Kolla in vårat utbud av posters" buttonHref="/posters/all-products" btnText="Alla Posters" />
